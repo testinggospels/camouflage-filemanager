aec9b2fef4ad076c3a5cf0a527c1455b32793c26
Shubhendu Madhukar <shubhendumadhukar@gmail.com>
2021-10-10 07:33:55 +0000

    feat: Add create file/folder endpoint

--- src/routes/api/fs/[...sub]/index.js
+++ src/routes/api/fs/[...sub]/index.js
@@ -1,4 +1,4 @@
-import { dirStat, deleteFile } from '$lib/fsutils';
+import { dirStat, deleteFile, createFolder, createFile } from '$lib/fsutils';
 
 export async function get({ params }) {
     const { sub } = params;
@@ -12,7 +12,7 @@ export async function del({ params }) {
     const { sub } = params;
     const response = await deleteFile(sub)
     let status = 200;
-    if (response.true) {
+    if (response.error) {
         status = 500
     }
     return {
@@ -22,4 +22,47 @@ export async function del({ params }) {
         }
     }
 
+}
+
+export async function post({ params, query }) {
+    const type = query.get("type");
+    const { sub } = params;
+    let status = 200;
+    let response = {};
+    if (type) {
+        switch (type) {
+            case "dir":
+                response = await createFolder(sub);
+                if (response.error) {
+                    status = 500
+                }
+                break;
+            case "file":
+                response = await createFile(sub);
+                if (response.error) {
+                    status = 500
+                }
+                break;
+            default:
+                response = { error: true, "message": "Incorrect 'type'" }
+                if (response.error) {
+                    status = 400
+                }
+                break;
+        }
+        return {
+            status: status,
+            body: {
+                response
+            }
+        }
+    } else {
+        return {
+            status: 400,
+            body: {
+                error: true,
+                message: "'type' missing or incorrect. Specify query param type=file or type=dir"
+            }
+        }
+    }
 }
