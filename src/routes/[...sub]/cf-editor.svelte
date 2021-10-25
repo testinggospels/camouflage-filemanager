413667a6df09b2a308506599beebd72f5ef0aa24
Shubhendu Madhukar <shubhendumadhukar@gmail.com>
2021-10-25 02:35:01 +0000

    fix: Add CodeMirror

--- src/routes/[...sub]/cf-editor.svelte
+++ src/routes/[...sub]/cf-editor.svelte
@@ -9,16 +9,37 @@
 
 <script>
 	import Breadcrumb from '$lib/components/Breadcrumb.svelte';
+	import { onMount } from 'svelte';
 	export let fileContent;
 	export let sub;
+	let editor;
+	onMount(() => {
+		let codeEditor = CodeMirror.fromTextArea(editor, {
+			lineNumbers: true,
+			theme: 'monokai'
+		});
+	});
 </script>
 
+<svelte:head>
+	<title>Camouflage - Editor</title>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.63.3/codemirror.min.js"></script>
+	<link
+		rel="stylesheet"
+		href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.63.3/codemirror.min.css"
+	/>
+	<link
+		rel="stylesheet"
+		href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.63.3/theme/monokai.min.css"
+	/>
+</svelte:head>
 <Breadcrumb sub="/{sub}" />
 <div class="p-6 h-96">
 	<textarea
 		class="textarea h-full w-full textarea-bordered textarea-primary"
 		placeholder="Mock File Content"
 		bind:value={fileContent}
+		bind:this={editor}
 	/>
 	<div class="btn-group fluid justify-center">
 		<button class="btn btn-neutral w-1/3 m-6">Cancel</button>
