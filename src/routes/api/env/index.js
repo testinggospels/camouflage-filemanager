e3af9e575b7a0a34049e69a3a36dca3c92aa490b
Shubhendu Madhukar <shubhendumadhukar@gmail.com>
2021-11-02 15:16:29 +0530

    fix: env endpoint

--- src/routes/api/env/index.js
+++ src/routes/api/env/index.js
@@ -1,7 +1,7 @@
 export async function get() {
     const env = {
-        FS_ROOT: process.env["FS_ROOT"],
-        WRITE_PROTECTED: process.env["WRITE_PROTECTED"],
+        FS_ROOT: process.env["FS_ROOT"] ? process.env["FS_ROOT"] : process.cwd(),
+        WRITE_PROTECTED: process.env["WRITE_PROTECTED"] ? process.env["WRITE_PROTECTED"] : 'false',
     }
     return {
         status: 200,
